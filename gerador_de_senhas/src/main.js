43402a312ee4698be9789096650874ad8b18ec6d
vinibol22 <63822714+vinibol22@users.noreply.github.com>
2021-01-20 20:17:30 -0300

    gerador de senhas

---
+++ gerador_de_senhas/src/main.js
@@ -0,0 +1,2 @@
+import formGeraSenha from './modules/formGeraSennha'
+import './assets/css/style.css';
---
+++ gerador_de_senhas/src/modules/formGeraSennha.js
@@ -0,0 +1,7 @@
+import geraSenha from './geradores'
+    const senhaGerada=document.quer
+
+export default ()=>{
+    console.log('Olá mundão')
+
+}
